ba56a391d835f96b1d41783078427ff16390f057
BaeJungYeon <qowjdduswkd@naver.com>
2023-06-06 20:41:30 +0900

    [Fix]Update CounselListImpl

--- src/ListImpl/CounselListImpl.java
+++ src/ListImpl/CounselListImpl.java
@@ -47,8 +47,19 @@ public class CounselListImpl implements CounselList,Remote {
 	}
 
 	public void update(Counsel updateCounsel) throws Exception {
-		counselDao.updateCounsel(updateCounsel);
-	}
+      for (int i = 0; i < counselList.size(); i++) {
+         Counsel counsel = counselList.get(i);
+         if (counsel.matchId(updateCounsel.getCounselID())) {
+            counsel.setCounselID(updateCounsel.getCounselID());
+            counsel.setCustomerID(updateCounsel.getCustomerID());
+            counsel.setContent(updateCounsel.getContent());
+            counsel.setDateOfCounsel(updateCounsel.getDateOfCounsel());
+            counsel.setManagerName(updateCounsel.getManagerName());
+         }
+            
+      }
+      counselDao.updateCounsel(updateCounsel);
+   }
 	public Counsel getCounselbyId(String customerID) {
 		for (int i = 0; i < this.counselList.size(); i++) {
 			Counsel counsel = (Counsel) this.counselList.get(i);
